7aa089e2723ae2b06dde4bb56d777eb0fb5aeff6
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2021-07-01 11:32:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-# script to configure and install KISS Linux on a Linode VPS
+#
+# Install KISS Linux OS on a Linode VPS
+# Author: Michael Czigler
+
+# set global variables
 ver=2021.5-1
 url=https://github.com/kiss-community/repo/releases/download/$ver
 giturl=
-hn=mcpcpc
+user=mcpcpc
 sdX=sda
 
+# format and partition drive, sdX
 cd $HOME
 mkfs.ext4 -F /dev/sda1
 mkfs.ext4 -F /dev/sda3
 mkfs.vfat -F 32 /dev/sda2
+
+# prepare drive for chroot/boostrap process
 mount /dev/sda3 /mnt
 wget "$url/kiss-chroot-$ver.tar.xz" 
 tar xvf kiss-chroot-$ver.tar.xz -C /mnt --strip-components 1
@@ -18,7 +25,10 @@ mkdir /mnt/boot/efi
 mount /dev/sda2 /mnt/boot/efi
 echo -e "/dev/sda1\t\t/boot\t\tvfat\t\tnoauto,noatime\t1 2" >> /mnt/etc/fstab
 echo -e "/dev/sda3\t\t/\t\text4\t\tnoatime\t\t0 1" >> /mnt/etc/fstab
-echo "$hn" >> /mnt/etc/hostname
+echo "$user" >> /mnt/etc/hostname
+
+# chroot/bootstrap KISS Linux, setup new user 
+# note: variables will not work here. All paths should be absolute.
 /mnt/bin/kiss-chroot /mnt <<"EOT"
 export CFLAGS="-O1 -pipe -march=native"
 export CXXFLAGS="-O1 -pipe -march=native"
@@ -32,11 +42,14 @@ kiss b eudev && kiss i eudev
 kiss b libelf && kiss i libelf
 kiss b ncurses && kiss i ncurses
 kiss b openssh && kiss i openssh
-adduser mc
-addgroup mc video
-addgroup mv audio
-#mkdir ~/repos
-#cd ~/repos
-#git clone https://github.com/kiss-community/repo  
 echo $$
 EOT
+
+## post installation steps (e.g. after reboot
+# adduser mc
+# addgroup mc video
+# addgroup mv audio
+# su mc
+# mkdir $HOME/repos
+# cd $HOME/repos
+git clone https://github.com/kiss-community/repo  
