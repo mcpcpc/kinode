57a6c04220571ef0e599be8b841b9bf94a20a602
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2021-07-22 08:19:28 -0400

    Update bootstrap.sh

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -10,6 +10,7 @@ kiss b libelf
 kiss b openssh
 kiss b grub
 kiss b dhcpcd
+kiss b baseinit
 cd /usr/src/linux-* && patch -p1 < kernel-no-perl.patch
 make -j1
 make INSTALL_MOD_STRIP=1 modules_install
@@ -21,7 +22,6 @@ mv grub grub.bak
 mv /root/grub ./
 grub-install --force /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-kiss b baseinit && kiss i baseinit
 ln -s /etc/sv/udevd/ /var/service
 ln -s /etc/sv/sshd/ /var/service
 ls -s /etc/sv/dhcpcd/ /var/service
